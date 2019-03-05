ee8b5b03e753077d1cf032bcd43a9b4d0af5fd1d
Kiyo <kiyohito.kunii@quantumblack.com>
2019-03-05 00:09:50 +0000

    Add input_form example

---
+++ input_form/hello.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, render_template
+
+app = Flask("MyApp")
+
+@app.route("/hello")
+def show_hello ():
+    return render_template("hello.html")
+
+@app.route("/signup", methods=["POST"])
+def sign_up():
+    form_data=request.form
+    print form_data["email"]
+    return "All OK"
+
+
+app.run(debug=True)
